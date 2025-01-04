cc3dc88790277cc77d4b14c6e1296ae942267b9a
Amund Eggen Svandal <aesvandal@gmail.com>
2025-01-04 14:33:16 +0100

    chore: Boilerplate and setup

--- eslint.config.js
+++ eslint.config.js
@@ -4,28 +4,43 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import reactCompiler from "eslint-plugin-react-compiler";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    { ignores: ["dist", "src/routeTree.gen.ts"] },
+    {
+        settings: { react: { version: "detect" } },
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.strictTypeChecked,
+            ...tseslint.configs.stylisticTypeChecked,
+        ],
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+            "react-compiler": reactCompiler,
+            react: react,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...react.configs["jsx-runtime"].rules,
+            "react-compiler/react-compiler": "error",
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+        },
+        languageOptions: {
+            parserOptions: {
+                project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "react-compiler": reactCompiler,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-compiler/react-compiler": "error",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
-  },
 );
--- vite.config.ts
+++ vite.config.ts
@@ -2,47 +2,49 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // import { reactClickToComponent } from "vite-plugin-react-click-to-component";
 import path from "node:path";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      react(),
-      /*
+    return {
+        plugins: [
+            TanStackRouterVite(),
+            react(),
+            /*
       (mode === "development" ||
         mode === "proxy-staging" ||
         mode === "proxy-production") &&
         reactClickToComponent(),
         */
-    ],
-    /**
-     * Import alias, enables imports such as
-     * import { AppLayout } from "@/components/Layouts";
-     * rather than relative imports.
-     *
-     * Should be used in conjunction with the `"paths"` field in `tsconfig.json`
-     */
-    resolve: {
-      alias: { "@": path.resolve(__dirname, "src") },
-    },
-    ...((mode === "proxy-staging" || mode === "proxy-production") && {
-      server: {
-        port: 5173,
-        open: true,
-        proxy: {
-          "/flashlight": {
-            target:
-              mode === "proxy-production"
-                ? "https://flashlight.recdep.com"
-                : "https://flashlight-test.recdep.com",
-            changeOrigin: true,
-            cookieDomainRewrite: {
-              "*": "localhost",
-            },
-            rewrite: (path) => path.replace(/^\/flashlight\//, "/"),
-          },
+        ],
+        /**
+         * Import alias, enables imports such as
+         * import { AppLayout } from "@/components/Layouts";
+         * rather than relative imports.
+         *
+         * Should be used in conjunction with the `"paths"` field in `tsconfig.json`
+         */
+        resolve: {
+            alias: { "@": path.resolve(import.meta.dirname, "src") },
         },
-      },
-    }),
-  };
+        ...((mode === "proxy-staging" || mode === "proxy-production") && {
+            server: {
+                port: 5173,
+                open: true,
+                proxy: {
+                    "/flashlight": {
+                        target:
+                            mode === "proxy-production"
+                                ? "https://flashlight.recdep.com"
+                                : "https://flashlight-test.recdep.com",
+                        changeOrigin: true,
+                        cookieDomainRewrite: {
+                            "*": "localhost",
+                        },
+                        rewrite: (path) => path.replace(/^\/flashlight\//, "/"),
+                    },
+                },
+            },
+        }),
+    };
 });
