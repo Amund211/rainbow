686c93a4096c40339823462fbedc910d53aac2ac
Amund Eggen Svandal <aesvandal@gmail.com>
2025-02-16 18:03:12 +0100

    chore: Use label getters to ensure consistency

--- src/routes/session.lazy.tsx
+++ src/routes/session.lazy.tsx
@@ -307,7 +307,7 @@ const ProgressionCaption: React.FC<ProgressionCaptionProps> = ({
         case "fkdr":
             return (
                 <Typography variant="caption">
-                    {`${progression.progressPerDay.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}/day (${progression.sessionFKDR.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} session FKDR, ${progression.finalKillsPerDay.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} final kills/day, ${progression.finalDeathsPerDay.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} final deaths/day)`}
+                    {`${progression.progressPerDay.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}/day (${progression.sessionFKDR.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} session ${getShortStatLabel("fkdr")}, ${progression.finalKillsPerDay.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ${getShortStatLabel("finalKills")}/day, ${progression.finalDeathsPerDay.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ${getShortStatLabel("finalDeaths")}/day)`}
                 </Typography>
             );
         case "kdr":
