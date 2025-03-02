44e06ef1e129365b90c2fc15b95bd3cfa065c4a0
Amund Eggen Svandal <aesvandal@gmail.com>
2025-03-02 21:11:30 +0100

    feat: Allow searching for usernames in search bar

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,26 +1,70 @@
+import { getUUIDQueryOptions, normalizeUUID } from "#queries/uuid.ts";
 import { Search } from "@mui/icons-material";
-import { InputAdornment, OutlinedInput } from "@mui/material";
+import { CircularProgress, InputAdornment, OutlinedInput } from "@mui/material";
+import { useQueryClient } from "@tanstack/react-query";
+import React from "react";
 
 interface UserSearchProps {
     onSubmit: (uuid: string) => void;
 }
 
+const isUUID = (value: string) => {
+    try {
+        normalizeUUID(value);
+    } catch {
+        return false;
+    }
+    return true;
+};
+
 export const UserSearch: React.FC<UserSearchProps> = ({ onSubmit }) => {
+    const queryClient = useQueryClient();
+
+    const [search, setSearch] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+
     return (
         <OutlinedInput
+            value={search}
+            onChange={(event) => {
+                setSearch(event.currentTarget.value);
+            }}
             placeholder="Search user..."
             startAdornment={
                 <InputAdornment position="start">
-                    <Search fontSize="small" />
+                    {loading ? (
+                        <CircularProgress size={20} />
+                    ) : (
+                        <Search fontSize="small" />
+                    )}
                 </InputAdornment>
             }
             fullWidth
             size="small"
             onKeyDown={(event) => {
                 if (event.key === "Enter") {
-                    // TODO: Convert from username to UUID
-                    const uuid = event.currentTarget.value;
-                    onSubmit(uuid);
+                    const value = event.currentTarget.value;
+
+                    // Allow UUIDs to be entered directly
+                    if (isUUID(value)) {
+                        onSubmit(value);
+                        setSearch("");
+                        return;
+                    }
+
+                    setLoading(true);
+                    queryClient
+                        .fetchQuery(getUUIDQueryOptions(value))
+                        .then(({ uuid }) => {
+                            onSubmit(uuid);
+                            setSearch("");
+                        })
+                        .catch((error: unknown) => {
+                            console.error("Failed to fetch username", error);
+                        })
+                        .finally(() => {
+                            setLoading(false);
+                        });
                 }
             }}
         />
