ec151c52e8d10b8ad0996af8b4bffbdad2348109
Amund Eggen Svandal <aesvandal@gmail.com>
2025-03-31 18:04:58 +0200

    chore: Add tests for endOfX

--- src/intervals.test.ts
+++ src/intervals.test.ts
@@ -2,9 +2,12 @@ import test from "node:test";
 import assert from "node:assert";
 
 import {
+    endOfDay,
     endOfLastDay,
     endOfLastMonth,
     endOfLastWeek,
+    endOfMonth,
+    endOfWeek,
     timeIntervalsFromDefinition,
 } from "#intervals.ts";
 
@@ -74,6 +77,103 @@ await test("getTimeIntervals", async (t) => {
 });
 
 await test("time helpers", async (t) => {
+    await t.test("endOfDay", async (t) => {
+        const cases = [
+            {
+                date: new Date(2024, 1, 14, 0, 0, 0),
+                expected: new Date(2024, 1, 14, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2024, 1, 14, 17, 15, 0),
+                expected: new Date(2024, 1, 14, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2024, 1, 14, 23, 59, 59, 999),
+                expected: new Date(2024, 1, 14, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2024, 1, 1, 0, 0, 0),
+                expected: new Date(2024, 1, 1, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2024, 1, 1, 0, 0, 0),
+                expected: new Date(2024, 1, 1, 23, 59, 59, 999),
+            },
+        ];
+        for (const { date, expected } of cases) {
+            await t.test(date.toISOString(), () => {
+                assert.strictEqual(
+                    endOfDay(date).toISOString(),
+                    expected.toISOString(),
+                );
+            });
+        }
+    });
+
+    await t.test("endOfWeek", async (t) => {
+        const cases = [
+            ...new Array(7).fill(null).map((_, i) => ({
+                // All days in the week point to the same end of last week
+                date: new Date(2024, 1, 12 + i, 17, 15, 0),
+                expected: new Date(2024, 1, 18, 23, 59, 59, 999),
+            })),
+            ...new Array(7).fill(null).map((_, i) => ({
+                // All days in the week point to the same end of last week
+                date: new Date(2024, 0, 1 + i, 0, 0, 0),
+                expected: new Date(2024, 0, 7, 23, 59, 59, 999),
+            })),
+        ];
+        for (const { date, expected } of cases) {
+            await t.test(date.toISOString(), () => {
+                assert.strictEqual(
+                    endOfWeek(date).toISOString(),
+                    expected.toISOString(),
+                );
+            });
+        }
+    });
+
+    await t.test("endOfMonth", async (t) => {
+        const cases = [
+            {
+                date: new Date(2024, 0, 1, 0, 0, 0),
+                expected: new Date(2024, 0, 31, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2024, 0, 14, 17, 15, 0),
+                expected: new Date(2024, 0, 31, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2024, 0, 29, 23, 59, 59, 999),
+                expected: new Date(2024, 0, 31, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2023, 11, 1, 0, 0, 0),
+                expected: new Date(2023, 11, 31, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2023, 11, 31, 23, 40, 0),
+                expected: new Date(2023, 11, 31, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2025, 1, 27, 18, 40, 0),
+                expected: new Date(2025, 1, 28, 23, 59, 59, 999),
+            },
+            {
+                date: new Date(2025, 1, 11, 23, 40, 0),
+                expected: new Date(2025, 1, 28, 23, 59, 59, 999),
+            },
+        ];
+        for (const { date, expected } of cases) {
+            await t.test(date.toISOString(), () => {
+                assert.strictEqual(
+                    endOfMonth(date).toISOString(),
+                    expected.toISOString(),
+                );
+            });
+        }
+    });
+
     await t.test("endOfLastDay", async (t) => {
         const cases = [
             {
