0ea5716b002e0e81e35e16fda9fc348cb42c9193
Amund Eggen Svandal <aesvandal@gmail.com>
2025-05-04 00:05:43 +0200

    feat: Add /session route
    Just a search bar for now

---
+++ src/routes/session/index.tsx
@@ -0,0 +1,47 @@
+import { UserSearch } from "#components/UserSearch.tsx";
+import { Stack } from "@mui/material";
+import { createFileRoute } from "@tanstack/react-router";
+import { usePlayerVisits } from "#contexts/PlayerVisits/hooks.ts";
+
+export const Route = createFileRoute("/session/")({
+    component: RouteComponent,
+});
+
+function RouteComponent() {
+    const navigate = Route.useNavigate();
+    const { visitPlayer } = usePlayerVisits();
+
+    return (
+        <Stack spacing={1}>
+            <meta
+                name="description"
+                content={`Search for player's session stats, including daily, weekly, and monthly stats, as well as a progression towards stat milestones, and individual session breakdowns.`}
+            />
+            <link rel="canonical" href={`https://prismoverlay.com/session`} />
+            <UserSearch
+                onSubmit={(uuid) => {
+                    visitPlayer(uuid);
+                    navigate({
+                        from: "/session",
+                        to: "/session/$uuid",
+                        params: { uuid },
+                        search: {
+                            gamemode: "overall",
+                            stat: "fkdr",
+                            timeIntervalDefinition: { type: "contained" },
+                            variantSelection: "both",
+                            sessionTableMode: "total",
+                            showExtrapolatedSessions: false,
+                        },
+                    }).catch((error: unknown) => {
+                        // TODO: Handle error
+                        console.error(
+                            "Failed to update search params: uuid",
+                            error,
+                        );
+                    });
+                }}
+            />
+        </Stack>
+    );
+}
