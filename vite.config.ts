cba8dcb3741016e7318d8628b58e43b46de010a6
Amund Eggen Svandal <aesvandal@gmail.com>
2025-05-13 21:45:00 +0200

    chore: Initialize sentry

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 import { reactClickToComponent } from "vite-plugin-react-click-to-component";
 import path from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     return {
+        build: {
+            sourcemap: true,
+        },
         plugins: [
             TanStackRouterVite({
                 addExtensions: true,
@@ -21,6 +25,20 @@ export default defineConfig(({ mode }) => {
                 mode === "proxy-staging" ||
                 mode === "proxy-production") &&
                 reactClickToComponent(),
+            sentryVitePlugin({
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+                org: "prism-overlay",
+                project: "rainbow",
+                bundleSizeOptimizations: {
+                    excludeDebugStatements: true,
+                    // Only relevant if you added `browserTracingIntegration`
+                    excludeTracing: true,
+                    // Only relevant if you added `replayIntegration`
+                    excludeReplayIframe: true,
+                    excludeReplayShadowDom: true,
+                    excludeReplayWorker: true,
+                },
+            }),
         ],
         /**
          * Import alias, enables imports such as
