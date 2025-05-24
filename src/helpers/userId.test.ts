368740c53e5dbd4be3c1e1694222a50b3b37e0e9
Amund Eggen Svandal <aesvandal@gmail.com>
2025-05-24 18:14:02 +0200

    chore: Add helpers to get and set user ids for browsers
    Will be sent with requests to the backend and in sentry reports. Can be
    useful to track usage and correlate logs with errors.

---
+++ src/helpers/userId.test.ts
@@ -0,0 +1,80 @@
+import test from "node:test";
+import { newUserId, validateUserId } from "./userId.ts";
+import assert from "node:assert";
+import { isNormalizedUUID } from "./uuid.ts";
+
+await test("newUserId", async (t) => {
+    await t.test(
+        "should generate a new user ID according to the spec",
+        async (t) => {
+            for (let i = 0; i < 10; i++) {
+                const userId = newUserId();
+                await t.test(userId, () => {
+                    assert.ok(
+                        userId.startsWith("rnb_"),
+                        "User ID should start with 'rnb_'",
+                    );
+
+                    const suffix = userId.slice(4);
+
+                    assert.ok(
+                        isNormalizedUUID(suffix),
+                        "User ID suffix should be a normalized UUID",
+                    );
+
+                    assert.ok(
+                        validateUserId(userId),
+                        "User ID should be valid",
+                    );
+                });
+            }
+        },
+    );
+
+    await t.test("should generate a new user ID according to the spec", () => {
+        const userId = newUserId();
+        assert.ok(
+            userId.startsWith("rnb_"),
+            "User ID should start with 'rnb_'",
+        );
+
+        const suffix = userId.slice(4);
+
+        assert.ok(
+            isNormalizedUUID(suffix),
+            "User ID suffix should be a normalized UUID",
+        );
+    });
+});
+
+await test("validateUserId", async (t) => {
+    const cases = [
+        {
+            input: "rnb_4a6113bb-5f94-4bc6-9e71-fc94e90c1fb2",
+            expected: true,
+        },
+        {
+            input: "rnb_custom_suffix",
+            expected: true,
+        },
+        {
+            input: null,
+            expected: false,
+        },
+        {
+            input: "invalid_prefix_0123456789abcdef0123456789abcdef",
+            expected: false,
+        },
+        {
+            input: "Rnb_invalid_prefix",
+            expected: false,
+        },
+    ];
+
+    for (const tc of cases) {
+        await t.test(tc.input ?? "<null>", () => {
+            const result = validateUserId(tc.input);
+            assert.strictEqual(result, tc.expected);
+        });
+    }
+});
