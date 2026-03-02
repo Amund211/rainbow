e8e6d0e56289ab768f72ae7b46374bf253515852
Amund Eggen Svandal <aesvandal@gmail.com>
2026-03-02 21:32:14 +0100

    chore: Verify label type assumption
    The type of the label formatter changes in the next version of recharts,
    so it is nice to get this in there before then.

---
+++ src/hooks/useAssumption.ts
@@ -0,0 +1,37 @@
+import { captureMessage } from "@sentry/react";
+import React from "react";
+
+export function useAssume() {
+    const assumptions = React.useRef<Record<string, boolean | undefined>>({});
+
+    return React.useCallback(function assume(
+        assumption: boolean,
+        message: string,
+        getMeta?: () => Record<string, unknown>,
+    ) {
+        if (assumption) {
+            // Assumption holds -> continue
+            return;
+        }
+
+        if (assumptions.current[message]) {
+            // We've already reported this assumption failure
+            return;
+        }
+
+        const meta = getMeta?.();
+
+        captureMessage(`Assumption failed: ${message}`, {
+            level: "error",
+            extra: meta,
+        });
+
+        if (import.meta.env.DEV) {
+            alert(
+                `Assumption failed: ${message}\n\n${JSON.stringify(meta, null, 2)}`,
+            );
+        }
+
+        assumptions.current[message] = true;
+    }, []);
+}
