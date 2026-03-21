f7fa5665f16fccd06546893e63b9934c4b008165
Amund Eggen Svandal <aesvandal@gmail.com>
2026-03-22 00:29:09 +0100

    WIP: prettify
    TODO: Localstorage write with event so we can capture cross hook changes
    on the same page
    
    TODO: Consider moving to indexeddb or somethiing? can localstorage be
    disabled somehow etc?

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -2,18 +2,18 @@ import React from "react";
 
 const makeSubscribe = (
     key: string,
-    onSubscribe: (callback: () => void) => void,
+    onSubscribe: (onStoreChange: () => void) => void,
     onUnsubscribe: () => void,
 ) => {
-    return (callback: () => void) => {
-        onSubscribe(callback);
+    return (onStoreChange: () => void) => {
+        onSubscribe(onStoreChange);
 
         const listener = (e: StorageEvent) => {
             // `key` is `null` on clear -> process it
             // https://developer.mozilla.org/en-US/docs/Web/API/Window/storage_event#event_properties
             if (e.key !== key && e.key != null) return;
 
-            callback();
+            onStoreChange();
         };
 
         window.addEventListener("storage", listener);
@@ -31,26 +31,28 @@ const makeGetSnapshot = (key: string) => {
     };
 };
 
-export const useLocalStorage = (key: string): [string | null, () => void] => {
-    const [refresh, setRefresh] = React.useState<() => void>(() => () => {
-        // no-op
-    });
+function noop() {
+    // no-op
+}
 
-    const subscribe = React.useMemo(
-        () =>
-            makeSubscribe(
-                key,
-                (callback: () => void) => {
-                    setRefresh(() => callback);
-                },
-                () => {
-                    setRefresh(() => () => {
-                        // no-op
-                    });
-                },
-            ),
-        [key],
-    );
+/**
+ * @returns [value, refresh], where `value` is the current value of the localStorage item, and `refresh` is a function that can be called to imperatively refresh the value from the outside.
+ */
+export const useLocalStorage = (key: string): [string | null, () => void] => {
+    // Keep the onStoreChange callback so we can imperatively refresh from the outside
+    // T
+    const [refresh, setRefresh] = React.useState<() => void>(() => noop);
+
+    const subscribe = React.useMemo(() => {
+        const onSubscribe = (onStoreChange: () => void) => {
+            setRefresh(() => onStoreChange);
+        };
+        const onUnsubscribe = () => {
+            setRefresh(() => noop);
+        };
+        return makeSubscribe(key, onSubscribe, onUnsubscribe);
+    }, [key]);
+
     const getSnapshot = React.useMemo(() => makeGetSnapshot(key), [key]);
 
     return [React.useSyncExternalStore(subscribe, getSnapshot), refresh];
