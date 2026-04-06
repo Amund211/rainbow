85cf478c00db33aea492b9330373afcfe159eb30
Amund Eggen Svandal <aesvandal@gmail.com>
2026-04-06 22:58:10 +0200

    chore(deps-dev): Bump @vitejs/plugin-react from 5.1.4 to 6.0.1
    Also bump babel-plugin-react-compiler, install @rolldown/plugin-babel,
    and move to the new way of using the react compiler with the react
    plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { reactClickToComponent } from "vite-plugin-react-click-to-component";
 import path from "node:path";
 import tanstackRouter from "@tanstack/router-plugin/vite";
@@ -16,10 +17,9 @@ export default defineConfig(({ mode }) => {
                 addExtensions: true,
                 autoCodeSplitting: true,
             }),
-            react({
-                babel: {
-                    plugins: ["babel-plugin-react-compiler"],
-                },
+            react(),
+            babel({
+                presets: [reactCompilerPreset()],
             }),
             (mode === "development" ||
                 mode === "proxy-staging" ||
